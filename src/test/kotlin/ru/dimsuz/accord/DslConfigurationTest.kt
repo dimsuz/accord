db4f68232f9e05e29e20ea600dfe9132ba6ac339
Dmitry Suzdalev <dimsuz@gmail.com>
2019-12-09 00:30:30 +0200

    Setup testing with junit + truth

---
+++ src/test/kotlin/ru/dimsuz/accord/DslConfigurationTest.kt
@@ -0,0 +1,11 @@
+package ru.dimsuz.accord
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class DslConfigurationTest {
+  @Test
+  fun check() {
+    assertThat(true).isTrue()
+  }
+}
